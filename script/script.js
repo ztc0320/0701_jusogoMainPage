2741f4759e07fdc16dc3b19e53e513ab84867c84
ztc0320 <rjdrjsdn@naver.com>
2021-07-02 12:16:40 +0900

    button click form submit

--- script/script.js
+++ script/script.js
@@ -36,4 +36,22 @@ $(function(){
         $("span#closeMark").css({ "visibility" : "hidden"});
     });
         /* X 표시 클릭하면 입력값을 없애고 X 표시도 숨김 */
+
+        /* 검색창이 공백일 때 검색 실행 버튼을 누르면
+            경고창을 발생하여 내용을 입력하게 만든다.
+            이렇게 사용자에게 올바른 행동을 유도하는 코드를 작성하는 것을
+            유효성 검사 라고 한다. */
+    $("span#searchMark").click(function(){
+        var txt = $("input#searchWord").val();
+        if(txt == ""){
+            alert("검색어을(를) 입력해주세요!");
+        }else{
+            $("form").submit();
+        }
+
+        // 텍스트박스에 포커스 설정하기
+        $("input#searchWord").focus();
+    });
+        /* 검색창 실행 */
+
     });
